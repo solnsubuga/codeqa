5efb4748b7cce8cc39c0856f4a96aa1089960b3b
Nsubuga Solomon <snsubuga@live.com>
2019-01-07 20:16:43 +0300

    feat(authentication): Add authentication
    - add login component
    - add form submission

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import { Home, SingleQuestion } from './components';
+import { Home, SingleQuestion, Login } from './components';
 
 const App = () => (
   <BrowserRouter>
     <div>
       <Route exact path="/" component={Home} />
       <Route exact path="/question/:id" component={SingleQuestion} />
+      <Route exact path="/login" component={Login} />
     </div>
   </BrowserRouter>
 );
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as QuestionForm } from './QuestionForm';
 export { default as Questions } from './Questions';
 export { default as Home } from './Home';
 export { default as SingleQuestion } from './SingleQuestion';
+export { default as Login } from './Login';
